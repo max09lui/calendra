49a7b486e0c969029e329971ed3de9f122d305a1
Michael <max09lui@gmail.com>
2025-07-18 01:50:24 -0700

    added fix

--- app/(main)/(private)/events/page.tsx
+++ app/(main)/(private)/events/page.tsx
@@ -3,6 +3,7 @@ import { CalendarPlus, CalendarRange } from "lucide-react";
 import Link from "next/link";
 import { auth } from "@clerk/nextjs/server";
 import { getEvents } from "@/server/actions/events";
+import EventCard from "@/components/cards/EventCard";
 
 export default async function EventsPage() {
 
@@ -35,7 +36,27 @@ export default async function EventsPage() {
                         </Button>
                 </div>
                 
-                
+                {/* Show event cards if any exist, otherwise show empty state */}
+            {events.length > 0 ? (
+              <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 p-10">
+                {events.map(event => (
+                  <EventCard key={event.id} {...event} />
+                ))}
+              </div>
+            ) : (
+              <div className="flex flex-col items-center gap-4">
+                <CalendarRange className="size-16 mx-auto text-black" />
+                You do not have any events yet. Create your first event to get
+                started!
+                <Button  
+                  className="bg-blue-500 hover:bg-blue-400 text-white py-6 hover:scale-110 duration-500 border-b-4 border-blue-700 hover:border-blue-500 rounded-2xl shadow-accent-foreground shadow-2xl text-2xl font-black"
+                  asChild>
+                  <Link href="/events/new">
+                    <CalendarPlus className="mr-4 size-7" /> New Event
+                  </Link>
+                </Button>
+              </div>
+            )}
                 
                 </section>
                 
--- schema/events.ts
+++ schema/events.ts
@@ -13,9 +13,11 @@ export const eventFormSchema = z.object({
   
     // 'durationInMinutes' will be coerced (converted) to a number
     // It must be an integer, greater than 0, and less than or equal to 720 (12 hours)
-    durationInMinutes: z.coerce
+    durationInMinutes: z.number()
+      /* 
       .number()
       .int()
       .positive("Duration must be greater than 0")
       .max(60 * 12, `Duration must be less than 12 hours (${60 * 12} minutes)`),
+      */
 })
